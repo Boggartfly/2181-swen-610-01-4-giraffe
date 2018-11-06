0f0382388c2f438d7398353f4424d64f3e233ba8
ac4922 <ac4922@rit.edu>
2018-11-05 23:37:05 -0500

    checkTurn

--- src/main/java/com/webcheckers/ui/CheckTurnRoute.java
+++ src/main/java/com/webcheckers/ui/CheckTurnRoute.java
@@ -16,14 +16,14 @@ public class CheckTurnRoute implements Route {
     @Override
     public Object handle(Request request, Response response) throws Exception {
 
-        String playerName=request.session().attribute("playerName");
+        String playerName=request.session().attribute("playerName").toString().trim();
 
         Game game = gameCenter.getPlayerGame(gameCenter.getPlayer(playerName));
 
-        if(game.getMyTurn() ==0 && game.getPlayer().getPlayerName() == playerName){
+        if(game.getMyTurn()==0 && game.getPlayer().getPlayerName() == playerName){
             return true;
         }
-        else if(game.getMyTurn() ==1 && game.getOpponent().getPlayerName() == playerName){
+        else if(game.getMyTurn()==1 && game.getOpponent().getPlayerName() == playerName){
             return true;
         }else {
             return false;
