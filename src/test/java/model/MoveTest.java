b2b68911cd672d90d25b52957f259783157dc5af
Parth Sane <laerdevstudios@gmail.com>
2018-11-05 23:18:25 -0500

    Added MoveTest.java

--- src/test/java/model/MoveTest.java
+++ src/test/java/model/MoveTest.java
@@ -1,4 +1,22 @@
 package model;
+import com.webcheckers.model.Move;
+import com.webcheckers.model.Position;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class MoveTest {
+
+    private Move move;
+    @Test
+    public void getStart(){
+        move.setStart(new Position(1,1));
+        assertEquals(new Position(1,1),move.getStart());
+    }
+
+    @Test
+    public void getEnd(){
+        move.setEnd(new Position(1,1));
+        assertEquals(new Position(1,1),move.getEnd());
+    }
 }
